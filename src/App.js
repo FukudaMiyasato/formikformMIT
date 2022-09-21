319bb6557f3ab535ee19891c30cc91eee6dfec89
fukudamiyasato <fukuda.miyasato@gmail.com>
2022-09-21 16:04:49 -0500

    final

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ function App() {
     },
     validate: values =>{
       let errors = {};
-      if(!values.email) errors.email='Field required';
+      if(!values.email) {errors.email='Field required'}
+      else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        errors.email='Username should be an email';
+      };
       if(!values.password) errors.password = 'Field required'; 
       return errors;
     }
@@ -35,7 +38,7 @@ function App() {
         <input id="emailField" name='email' type='text' onChange={formik.handleChange} value={formik.values.email}/>
         {formik.errors.email ? <div id="emailError" style={styleError}>{formik.errors.email}</div>:null}
         <div>Password :</div>
-        <input id="pswField" name='password' type='text' onChange={formik.handleChange} value={formik.values.password}/>
+        <input id="pswField" name='password' type='password' onChange={formik.handleChange} value={formik.values.password}/>
         {formik.errors.password ? <div id="pswError" style={styleError}>{formik.errors.password}</div>:null}
         <button id="submitBtn" type='submit'>Submit</button>
       </form>
